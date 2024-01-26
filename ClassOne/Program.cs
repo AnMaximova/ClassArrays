8b95a73f3124ee6d4d5713128fee5293144ce660
Anna Maximova <maksimova.anna@lit1533.ru>
2024-01-26 21:34:09 +0300

    Добавьте файлы проекта.

---
+++ ClassOne/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Xml.Linq;
+
+class ProgramOne
+{
+    class OneDimensionalArray
+    {
+        private int[] arr; //массив
+        private float average = 0; //среднее арифметическое
+
+        public OneDimensionalArray(int size, bool input_mode = false) //возможность заполнения массива пользователем 
+        {
+            arr = new int[size];
+            if (input_mode)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.Write($"Введите {i} элемент массива: ");
+                    arr[i] = int.Parse(Console.ReadLine());
+                    average += arr[i];
+                }
+            }
+            else
+            {
+                Random rnd = new Random();
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = rnd.Next(-200, 201);
+                    average += arr[i];
+                }
+            }
+            average /= arr.Length;
+        }
+
+        public void InputMethod() // публичный метод заполнения массива, удобно использовать при повторном заполнении массива
+        {
+            Random rnd = new Random();
+            average = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(-10, 11);
+                average += arr[i];
+            }
+            average /= arr.Length;
+        }
+
+        public void OutArrColumn() // вывод массива в столбик с номерами элементов
+        {
+            Console.WriteLine("Вывод массива");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"{i} элемент массива равен: {arr[i]}");
+            }
+        }
+
+        public void OutArrLine() // вывод массива в строку
+        {
+            Console.WriteLine("Вывод массива");
+            foreach (int element in arr)
+                Console.Write(element.ToString() + "\t");
+            Console.WriteLine();
+        }
+
+        public float Average //среднее арифметическое элементов массива
+        {
+            get
+            {
+                return average;
+            }
+        }
+
+        public void MoreThan100Abs() //удаление из массива больших чем 100 по модулю элементов
+        {
+            int num = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (Math.Abs(arr[i]) <= 100)
+                {
+                    num++;
+                }
+            }
+            int[] an = new int[num];
+            int j = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (Math.Abs(arr[i]) <= 100)
+                {
+                    an[j] = arr[i];
+                    j++;
+                }
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i < an.Length)
+                {
+                    arr[i] = an[i];
+                    Console.Write(arr[i].ToString() + "\t");
+                }
+                else 
+                {
+                    arr[i] = 0; 
+                }
+            }
+            Console.WriteLine("\n");
+        }
+
+        private void BubbleSort() // сортировка массива, используется для удаления повторяющихся элементов
+        {
+            int temp;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+        }
+        public void DelRep() //удаление повтояющихся элементов массива
+        {
+            int num = 1;
+            BubbleSort();
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] != arr[i + 1])
+                {
+                    num++;
+                }
+            }
+            int[] an = new int[num];
+            int j = 0;
+            an[j] = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] != an[j])
+                {
+                    j++;
+                    an[j] = arr[i];
+                }
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i < an.Length)
+                {
+                    arr[i] = an[i];
+                    Console.Write(arr[i].ToString() + "\t");
+                }
+                else
+                {
+                    arr[i] = 0;
+                }  
+            }
+        }
+    }
+
+    static void Main()
+    {
+        OneDimensionalArray one = new OneDimensionalArray(6);
+        one.OutArrColumn();
+        Console.WriteLine($"Среднее арифметическое элементов массива равно {one.Average}");
+        Console.WriteLine("Из массива удалены все элементы, большие 100 по модулю:");
+        one.MoreThan100Abs();
+        Console.WriteLine("Заполним снова массив через вызов отдельного публичного метода");
+        one.InputMethod();
+        one.OutArrLine();
+        Console.WriteLine($"Среднее арифметическое элементов массива равно {one.Average}");
+        Console.WriteLine("Заполните массив самостоятельно (3 элемента):");
+        OneDimensionalArray two = new OneDimensionalArray(3, true);
+        two.OutArrLine();
+        Console.WriteLine("Заполните массив самостоятельно (10 элементов):");
+        OneDimensionalArray three = new OneDimensionalArray(10, true);
+        Console.WriteLine("Из массива удалены все повторяющиеся элементы:");
+        three.DelRep(); 
+    }
+}
+
---
+++ ClassTwo/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+
+class ProgramTwo
+{
+    class TwoDimensionalArray
+    {
+        private int[,] arr; //массив
+        private float average = 0; //среднее арифметическое
+
+        public TwoDimensionalArray(int row, int column, bool input_mode = false) //возможность заполнения массива пользователем 
+        {
+            arr = new int[row, column];
+            if (input_mode)
+            {
+                for (int i = 0; i < arr.GetLength(0); i++)
+                {
+                    for (int j = 0; j < arr.GetLength(1); j++)
+                    {
+                        Console.Write($"Элемент [{i},{j}]: ");
+                        arr[i, j] = int.Parse(Console.ReadLine());
+                        average += arr[i, j];
+                    }
+                }
+            }
+            else
+            {
+                Random rnd = new Random();
+                for (int i = 0; i < arr.GetLength(0); i++)
+                {
+                    for (int j = 0; j < arr.GetLength(1); j++)
+                    {
+                        arr[i, j] = rnd.Next(-200, 201);
+                        average += arr[i, j];
+                    }
+                }
+            }
+            average /= (arr.GetLength(0) * arr.GetLength(1));
+        }
+
+        public void InputSplit() //ввод элементов массива построчно через пробел
+        {
+            average = 0;
+            Console.WriteLine("Вводите через пробел значения элементов массива по строкам:");
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                Console.WriteLine($"Введите через пробел элементы {i} строки:");
+                string text = Console.ReadLine();
+                string[] words = text.Split(' ');
+                if (words.Length != arr.GetLength(1))
+                {
+                    if (words.Length < arr.GetLength(1))
+                    {
+                        Console.WriteLine("Вы ввели меньше элементов, в конце будут нули");
+                        for (int j = 0; j < words.Length; j++)
+                        {
+                            arr[i, j] = int.Parse(words[j]);
+                            average += arr[i, j];
+                        }
+                        for (int j = words.Length; j < arr.GetLength(1); j++)
+                        {
+                            arr[i, j] = 0;
+                        }
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Вы ввели больше элементов, лишние будут отброшены");
+                    }
+                }
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = int.Parse(words[j]);
+                    average += arr[i, j];
+                }
+            }
+            average /= (arr.GetLength(0) * arr.GetLength(1));
+        }
+
+        public void OutArr() // вывод двумерного массива
+        {
+            Console.WriteLine("Вывод двумерного массива");
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write($"{arr[i, j]}\t");
+                }
+                Console.Write("\n");
+            }
+        }
+
+        public void OutArrEvenReverse() // вывод двумерного массива, четные строки в обратном порядке
+        {
+            Console.WriteLine("Вывод двумерного массива, четные строки в обратном порядке");
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    int index = j;
+                    if (i % 2 == 0)
+                    {
+                        index = arr.GetLength(1) - j - 1;
+
+                    }
+                    Console.Write($"{arr[i, index]}\t");
+                }
+                Console.Write("\n");
+            }
+        }
+
+        public float Average //среднее арифметическое элементов массива
+        {
+            get
+            {
+                return average;
+            }
+        }
+
+        public void Det() 
+        {
+            if (arr.GetLength(0) != arr.GetLength(1)) 
+            {
+                Console.WriteLine("Вычислить определитель можно только у квадратной матрицы");
+                return;
+            }
+            else
+            {
+                double[,] matrix = new double[arr.GetLength(0), arr.GetLength(0)];
+                double det = 1;
+                // копируем матрицу в тип double для преобразования к треугольной
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(0); j++)
+                    {
+                        matrix[i, j] = arr[i,j];
+                    }
+
+                }
+                // приводим матрицу к треугольному виду (алгоритм Гаусса)
+                for (int i = 0; i < matrix.GetLength(0) - 1; i++)
+                {
+                    for (int j = i + 1; j < matrix.GetLength(0); j++)
+                    {
+                        double koef = matrix[j, i] / matrix[i, i];
+                        for (int k = i; k < matrix.GetLength(0); k++)
+                            matrix[j, k] -= matrix[i, k] * koef;
+                    }
+                }
+                // выводим треугольную матрицу, чтобы увидеть, что она треугольная, и считаем определитель
+                Console.WriteLine("Треугольная матрица");
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(0); j++)
+                    {
+                        Console.Write("{0:f1}\t", matrix[i,j]); 
+                    }
+                    Console.WriteLine();
+                    det *= matrix[i, i];
+                }
+                Console.WriteLine("Определитель матрицы равен {0:f0}",det);
+            }
+        }
+    }
+    static void Main()
+    {
+        TwoDimensionalArray one = new TwoDimensionalArray(4, 6);
+        one.OutArr();
+        one.OutArrEvenReverse();
+        Console.WriteLine($"Среднее арифметическое элементов массива равно {one.Average}");
+        Console.WriteLine("Заполните двумерный массив самостоятельно (3 строки, два столбца):");
+        TwoDimensionalArray two = new TwoDimensionalArray(3, 2, true);
+        two.OutArr();
+        two.OutArrEvenReverse();
+        Console.WriteLine($"Среднее арифметическое элементов массива равно {two.Average}");
+        Console.WriteLine("Введите элементы массива через пробел (2 строки, 3 столбца):");
+        TwoDimensionalArray three = new TwoDimensionalArray(2,3);
+        three.InputSplit();
+        three.OutArr();
+        Console.WriteLine($"Среднее арифметическое элементов массива равно {three.Average}");
+        Console.WriteLine("Вычислим определитель матрицы");
+        TwoDimensionalArray four = new TwoDimensionalArray(3, 3);
+        four.OutArr();
+        four.Det();
+    }
+}
